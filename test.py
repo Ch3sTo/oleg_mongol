3ccfd9951ee767ce8060061bf977f0fd14d062d5
Ch3sTo <.>
2026-02-02 18:49:34 +0300

    улучшение функции

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 def test(some_tuple: tuple) -> list:
+    if type(some_tuple)!=tuple:
+        raise TypeError("нужен кортеж")
     some_list=[]
     for elem in some_tuple:
         some_list.append(elem)
     return some_list
 print(test((1, 2, 3 ,4,8 , 9 ,675 ,5 ,5 ,5 ,6, 7,7,)))
-print(())
+print(test("1"))
